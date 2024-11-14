82f000840e719902abe43536b7fb83d9c9b1229c
Dickgay <47469884+Dickgay@users.noreply.github.com>
2024-11-14 14:48:58 +0800

    Add files via upload

---
+++ public/settings.js
@@ -0,0 +1,7 @@
+var settings = {
+    telemetry_enabled: false,
+    url_dl: "/functions/download", // 下载测速路径
+    url_ul: "/functions/upload",   // 上传测速路径
+    url_ping: "/functions/ping",   // Ping 测试路径
+    url_getIp: "/functions/ip",    // 获取客户端 IP 地址路径
+};
