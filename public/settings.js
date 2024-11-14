46dfef1f62d7cc18eaf5c9fb3e72f03bf2f096a0
Dickgay <47469884+Dickgay@users.noreply.github.com>
2024-11-14 15:06:32 +0800

    Delete public directory

--- public/settings.js
+++
@@ -1,7 +0,0 @@
-var settings = {
-    telemetry_enabled: false,
-    url_dl: "/functions/download",
-    url_ul: "/functions/upload",
-    url_ping: "/functions/ping",
-    url_getIp: "/functions/ip",
-};
