966b1219aa9bfb202e498fb5477355f21395ecb6
Dickgay <47469884+Dickgay@users.noreply.github.com>
2024-11-14 15:02:10 +0800

    Delete functions directory

--- functions/ip.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-    const ip = context.request.headers.get("cf-connecting-ip");
-    return new Response(JSON.stringify({ ip }), {
-        headers: { "Content-Type": "application/json" },
-    });
-}
--- functions/upload.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-    const size = (await context.request.arrayBuffer()).byteLength;
-    return new Response("OK", {
-        headers: { "Content-Type": "text/plain" },
-    });
-}
